1226345c39d421ca6e0402c2ccee5bdd8f1e9f5d
tcarisland <tcarisland@gmail.com>
2024-03-19 10:58:21 +0100

    fixed failing method in tsfontconverter closes #2

--- src/tsfontconverter/tsfontconverter.py
+++ src/tsfontconverter/tsfontconverter.py
@@ -4,4 +4,4 @@ class TSFontConverter:
 
     def convert_font(self, font):
         # Add your font conversion logic here
-        print("converting font: {font}".format(str(font)))
+        print("converting font: {font}".format(font=str(font)))
---
+++ tests/tsfontconvertertest.py
@@ -0,0 +1,6 @@
+from src.tsfontconverter.tsfontconverter import TSFontConverter
+
+def test_tsfontconverter():
+    tsfc = TSFontConverter()
+    tsfc.convert_font("tuscan-modular.otf")
+
