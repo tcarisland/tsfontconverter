fe5d43e86d5216930d944d7ae104c067e631fc93
tcarisland <tcarisland@gmail.com>
2024-03-19 10:41:05 +0100

    renamed directories and refactoring #2

--- setup.py
+++ setup.py
@@ -3,15 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='tsfontconverter',
     version='0.1',
-    packages=find_packages('tsfontconverter'),
-    package_dir={'': 'tsfontconverter'},
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     install_requires=[
         # Add your dependencies here
     ],
     author='Thor Arisland',
     description='A Python package for converting fonts',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
     url='https://github.com/tcarisland/tsfontconverter',
     license='Apache License 2.0',
     classifiers=[
