500e53b692dd0bcb96263afbc5c18a54950418c7
tcarisland <tcarisland@gmail.com>
2024-03-19 07:46:09 +0100

    project initialized closes #1

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tsfontconverter',
+    version='0.1',
+    packages=find_packages('Lib'),
+    package_dir={'': 'Lib'},
+    install_requires=[
+        # Add your dependencies here
+    ],
+    author='Thor Arisland',
+    description='A Python package for converting fonts',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tcarisland/tsfontconverter',
+    license='Apache License 2.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+)
+
