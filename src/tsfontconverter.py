783aeb509e9cb1c9ce7661beb86609ab5e1b8c4c
tcarisland <tcarisland@gmail.com>
2024-03-20 09:53:41 +0100

    added tsfontconverter as static methods #3

---
+++ src/tsfontconverter.py
@@ -0,0 +1,11 @@
+from .fontparser.fontparser import FontParser
+
+
+def convert_font(font):
+    # Add your font conversion logic here
+    print("converting font: {font}".format(font=str(font)))
+
+
+def create_font_list(fontdirpath, outputpath):
+    fontparser = FontParser(fontdirpath=fontdirpath, outputpath=outputpath)
+    fontparser.list_font_files()
